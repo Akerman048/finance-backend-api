6e5cd96360d6d4211fdb186ad3344559b5be08ac
Valerii Oleksiienko <akerman8048@gmail.com>
2026-05-19 11:40:25 +0000

    Connect PostgreSQL database

--- src/app.js
+++ src/app.js
@@ -1,14 +1,24 @@
-import express from 'express';
-import cors from 'cors';
-
+import express from "express";
+import cors from "cors";
+import { pool } from "./db/pool.js";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.get('/',(req,res)=>{
-  res.json({message:'Finance API works'})
-})
+app.get("/", (req, res) => {
+  res.json({ message: "Finance API works" });
+});
+
+app.get("/transactions", async (req, res) => {
+  try {
+    const result = await pool.query("SELECT * FROM transactions");
+    res.json(result.rows);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server error" });
+  }
+});
 
 export default app;
