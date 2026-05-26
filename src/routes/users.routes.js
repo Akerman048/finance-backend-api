65d64082b40774e089730a0efcee4d58ecc04721
Valerii Oleksiienko <akerman8048@gmail.com>
2026-05-26 09:54:38 +0000

    Add monthly analytics

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -2,7 +2,6 @@ import express from "express";
 import {
   registerUser,
   loginUser,
-  getMonthlyAnalytics,
   refreshToken,
   logoutUser,
   getMe,
@@ -14,7 +13,6 @@ const router = express.Router();
 
 router.post("/register", registerUser);
 router.post("/login", loginLimiter, loginUser);
-router.get('/monthly', authMiddleware, getMonthlyAnalytics)
 router.post("/refresh", refreshToken);
 router.use("/logout", logoutUser);
 router.get("/me", authMiddleware, getMe);
